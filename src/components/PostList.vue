07beeb5cf3434b51cf28e052d7ace970648d600c
xiaolinzi7118 <1106147743@qq.com>
2021-05-26 19:19:53 +0800

    save

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="PostList">
-    <div class='loading' v-if="isLoading">
-      <img src="../assets/loading.gif">
+    <div class="loading" v-if="isLoading">
+      <img src="../assets/loading.gif" />
     </div>
     <!-- 主题帖子列表 -->
-    <div v-else class='posts'>
+    <div v-else class="posts">
       <ul>
         <li>
           <div class="toobar">
-          <span>全部</span>
-          <span>精华</span>
-          <span>分享</span>
-          <span>问答</span>
-          <span>招聘</span>
+            <span>全部</span>
+            <span>精华</span>
+            <span>分享</span>
+            <span>问答</span>
+            <span>招聘</span>
           </div>
         </li>
-        <li v-for='post in posts' :key='post.id'>
+        <li v-for="post in posts" :key="post.id">
           <!-- 头像 -->
-          <img :src='post.author.avatar_url' alt="">
+          <img :src="post.author.avatar_url" alt="" />
           <!-- 回复/浏览 -->
           <span>
-            <span class="reply_count"> {{post.reply_count}}</span>
-            /{{post.visit_count}}
+            <span class="reply_count"> {{ post.reply_count }}</span>
+            /{{ post.visit_count }}
           </span>
           <!-- 帖子的分类 -->
-          <span :class="{'put_good':post.good,'put_top':post.top,
-            'topiclist-tab':(post.good!=true && post.top!=true)}">
-          <span>
-            {{post | tabFormatter}}
-          </span>
+          <span
+            :class="{
+              put_good: post.good,
+              put_top: post.top,
+              'topiclist-tab': post.good != true && post.top != true,
+            }"
+          >
+            <span>
+              {{ post | tabFormatter }}
+            </span>
           </span>
           <!-- 标题 -->
-          <router-link :to="{
-            name:'post_content',
-            params:{
-              id:post.id,
-              name:post.author.loginname
-            }
-          }" >
+          <router-link
+            :to="{
+              name: 'post_content',
+              params: {
+                id: post.id,
+                name: post.author.loginname,
+              },
+            }"
+          >
             <span>
-              {{post.title}}
-          </span>
+              {{ post.title }}
+            </span>
           </router-link>
           <!-- 最终回复时间 -->
           <span class="last_reply">
-            {{post.last_reply_at | formatDate}}
+            {{ post.last_reply_at | formatDate }}
           </span>
         </li>
         <li>
@@ -56,162 +63,167 @@
 </template>
 
 <script>
-import NavPage from './NavPage'
-export default{
-  name:'PostList',
-  components:{
-    NavPage
+import NavPage from "./NavPage";
+export default {
+  name: "PostList",
+  components: {
+    NavPage,
   },
-  data(){
+  data() {
     return {
-      isLoading:false,
-      posts:[],
-      postpage:1
-      }
+      isLoading: false,
+      posts: [],
+      postpage: 1,
+      initRes: [],
+    };
   },
-  methods:{
-    getData(){
-      this.$http.get('https://cnodejs.org/api/v1/topics',{
-        params:{
-          page:this.postpage,
-          limit:20
-        }
-      })
-      .then(res=>{
-        this.isLoading=false;
-        this.posts=res.data.data;
-      })
-      .catch(err=>{
-        console.log(err);
-      })
+  methods: {
+    getData() {
+      this.$http
+        .get("https://cnodejs.org/api/v1/topics", {
+          params: {
+            page: this.postpage,
+            limit: 20,
+          },
+        })
+        .then((res) => {
+          this.isLoading = false;
+          this.posts = res.data.data;
+          this.initRes = res.data.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    renderList(value){
-      this.postpage=value;
+    renderList(value) {
+      this.postpage = value;
       this.getData();
-    }
+    },
   },
-  beforeMount(){
-    this.isLoading=true;
-    this.getData()
-  }
-}
+  beforeMount() {
+    this.isLoading = true;
+    this.getData();
+  },
+};
 </script>
 
 
 <style lang="scss" scoped>
-  .posts {
-    margin-top: 20px;
-  }
+.posts {
+  margin-top: 20px;
+}
 
-  .PostList img {
-    height: 30px;
-    width: 30px;
-    vertical-align: middle;
-  }
+.PostList img {
+  height: 30px;
+  width: 30px;
+  vertical-align: middle;
+}
 
-  ul {
-    list-style: none;
-    width: 100%;
-    max-width: 1344px;
-    margin: 0 auto;
-  }
+ul {
+  list-style: none;
+  width: 100%;
+  max-width: 1344px;
+  margin: 0 auto;
+}
 
-  ul li:not(:first-child) {
-    padding: 9px;
-    font-size: 15px;
-    font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma, "Hiragino Sans GB", STHeiti, sans-serif !important;
-    font-weight: 400;
-    background-color: white;
-    color: #333;
-    border-top: 1px solid #f0f0f0;
-  }
+ul li:not(:first-child) {
+  padding: 9px;
+  font-size: 15px;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma,
+    "Hiragino Sans GB", STHeiti, sans-serif !important;
+  font-weight: 400;
+  background-color: white;
+  color: #333;
+  border-top: 1px solid #f0f0f0;
+}
 
-  li:not(:first-child):hover {
-    background: #f5f5f5;;
-  }
+li:not(:first-child):hover {
+  background: #f5f5f5;
+}
 
-  li:last-child:hover {
-    background: white;
-  }
+li:last-child:hover {
+  background: white;
+}
 
-  li span {
-    line-height: 30px;
-  }
+li span {
+  line-height: 30px;
+}
 
-  .allcount {
-    width: 70px;
-    display: inline-block;
-    text-align: center;
-    font-size: 12px;
-  }
+.allcount {
+  width: 70px;
+  display: inline-block;
+  text-align: center;
+  font-size: 12px;
+}
 
-  .reply_count {
-    color: #9e78c0;
-    font-size: 14px;
-  }
+.reply_count {
+  color: #9e78c0;
+  font-size: 14px;
+}
 
-  .put_good, .put_top {
-    background: #80bd01;
-    padding: 2px 4px;
-    border-radius: 3px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    -o-border-radius: 3px;
-    color: #fff;
-    font-size: 12px;
-    margin-right: 10px;
-  }
+.put_good,
+.put_top {
+  background: #80bd01;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  color: #fff;
+  font-size: 12px;
+  margin-right: 10px;
+}
 
-  .topiclist-tab {
-    background-color: #e5e5e5;
-    color: #999;
-    padding: 2px 4px;
-    border-radius: 3px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    -o-border-radius: 3px;
-    font-size: 12px;
-    margin-right: 10px;
-  }
+.topiclist-tab {
+  background-color: #e5e5e5;
+  color: #999;
+  padding: 2px 4px;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  font-size: 12px;
+  margin-right: 10px;
+}
 
-  .last_reply {
-    text-align: right;
-    min-width: 50px;
-    display: inline-block;
-    white-space: nowrap;
-    float: right;
-    color: #778087;
-    font-size: 12px;
-  }
+.last_reply {
+  text-align: right;
+  min-width: 50px;
+  display: inline-block;
+  white-space: nowrap;
+  float: right;
+  color: #778087;
+  font-size: 12px;
+}
 
-  .toobar {
-    height: 40px;
-    background-color: #f5f5f5;
-  }
+.toobar {
+  height: 40px;
+  background-color: #f5f5f5;
+}
 
-  .toobar span {
-    font-size: 14px;
-    color: #80bd01;
-    line-height: 40px;
-    margin: 0 10px;
-    cursor: pointer;
-  }
+.toobar span {
+  font-size: 14px;
+  color: #80bd01;
+  line-height: 40px;
+  margin: 0 10px;
+  cursor: pointer;
+}
 
-  .toobar span:hover {
-    color: #9e78c0;
-  }
+.toobar span:hover {
+  color: #9e78c0;
+}
 
-  a {
-    text-decoration: none;
-    color: black;
-  }
+a {
+  text-decoration: none;
+  color: black;
+}
 
-  a:hover {
-    text-decoration: underline;
-  }
+a:hover {
+  text-decoration: underline;
+}
 
-  .loading {
-    text-align: center;
-    padding-top: 300px;
-  }
+.loading {
+  text-align: center;
+  padding-top: 300px;
+}
 </style>
